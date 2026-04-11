c54df56fa239403c2a4e9d1c63f91a054236b768
VitorNovaisv <vitornovaisv@gmail.com>
2026-04-10 21:52:45 -0300

    feat: Event creation and image uploading using s3 and supabase (EventController, EventService)

--- src/main/java/com/eventostec/api/service/EventService.java
+++ src/main/java/com/eventostec/api/service/EventService.java
@@ -2,19 +2,39 @@ package com.eventostec.api.service;
 
 import com.eventostec.api.domain.event.Event;
 import com.eventostec.api.domain.event.EventRequestDTO;
+import com.eventostec.api.repositories.EventRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
+import software.amazon.awssdk.core.sync.RequestBody;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 
 import java.util.Date;
+import java.util.UUID;
 
 @Service
 public class EventService {
-    public Event createEvent(EventRequestDTO data) {
-        String imgUrl = null;
 
-        if(data.image()!=null) {
-           imgUrl = this.uploadImg(data.image());
-        }
+    @Value("${supabase.s3.bucket-name}")
+    private String bucketName;
+
+    @Value("${supabase.s3.supabaseUrl}")
+    private String supabaseUrl;
+
+    @Autowired
+    private S3Client s3Client;
+
+
+    @Autowired
+    private EventRepository eventRepository;
+
+    public Event createEvent(EventRequestDTO data) {
+
+
+
+        String imgUrl = this.uploadImg(data.image());
 
         Event newEvent = new Event();
         newEvent.setTitle(data.title());
@@ -22,11 +42,33 @@ public class EventService {
         newEvent.setEventUrl(data.eventUrl());
         newEvent.setDate(new Date(data.date()));
         newEvent.setImgUrl(imgUrl);
+        newEvent.setRemote(data.remote());
+
+        eventRepository.save(newEvent);
 
         return newEvent;
     }
 
-    private String uploadImg(MultipartFile multipartFile) {
-        return "";
+    public String uploadImg(MultipartFile file){
+
+        try {
+            String fileName =  UUID.randomUUID().toString()+"."+file.getOriginalFilename();
+
+            PutObjectRequest putOb = PutObjectRequest.builder()
+                    .bucket(bucketName)
+                    .key(fileName)
+                    .contentType(file.getContentType())
+                    .build();
+
+            s3Client.putObject(putOb, RequestBody.fromBytes(file.getBytes()));
+
+            return String.format("%s/storage/v1/object/public/%s/%s",
+                    supabaseUrl, bucketName, fileName);
+
+        }catch (Exception e){
+            System.out.println("Erro ao enviar Imagem" + e);
+            return null ;
+        }
     }
+
 }
