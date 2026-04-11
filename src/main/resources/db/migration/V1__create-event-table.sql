82b17c2e395b1fae6a0f30d2fead108b6760747f
VitorNovaisv <vitornovaisv@gmail.com>
2026-04-10 21:50:42 -0300

    fix: updating table to fit Url generated stored in supabase

--- src/main/resources/db/migration/V1__create-event-table.sql
+++ src/main/resources/db/migration/V1__create-event-table.sql
@@ -4,7 +4,7 @@ CREATE TABLE events (
 id UUID DEFAULT gen_random_uuid() PRIMARY KEY,
     title VARCHAR(100) NOT NULL,
     description VARCHAR(250) NOT NULL,
-    img_url VARCHAR(100) NOT NULL,
+    img_url TEXT NOT NULL,
     event_url VARCHAR(100) NOT NULL,
     date TIMESTAMP NOT NULL,
     remote BOOLEAN NOT NULL
