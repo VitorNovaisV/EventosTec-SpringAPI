c67bedcfe0dce844f7fb08a57a173b845ae1d7b4
VitorNovaisv <vitornovaisv@gmail.com>
2026-04-14 15:18:06 -0300

    feat: Creating Coupon Entity and CouponRequestDTO.

--- src/main/java/com/eventostec/api/domain/coupon/Coupon.java
+++ src/main/java/com/eventostec/api/domain/coupon/Coupon.java
@@ -21,6 +21,7 @@ public class Coupon {
     @Id
     @GeneratedValue
     private UUID id;
+    private String code;
     private Integer discount;
     private Date valid;
 
---
+++ src/main/java/com/eventostec/api/domain/coupon/CouponRequestDTO.java
@@ -0,0 +1,8 @@
+package com.eventostec.api.domain.coupon;
+
+import com.eventostec.api.domain.event.Event;
+
+import java.util.Date;
+
+public record CouponRequestDTO(String code, Integer discount, Long valid) {
+}
